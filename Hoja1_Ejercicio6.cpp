2c70e5d368ee5e0fa5484b6119b24bbce6bc49b0
Augusto Morante <48858576+AugustoMorante@users.noreply.github.com>
2019-04-16 07:56:29 -0500

    Update Hoja1_Ejercicio6.cpp

--- Hoja1_Ejercicio6.cpp
+++ Hoja1_Ejercicio6.cpp
@@ -1,20 +1,22 @@
 
 #include "pch.h"
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main()
 {
-	int a, b, c, v, dis;
-	double r1, r2;
+	int a, b, c, x, dis;
+	double raiz, r1, r2, formula;
 	cout << "Ingrese valor de A: ";
 	cin >> a;
 	cout << "Ingrese valor de B: ";
 	cin >> b;
 	cout << "Ingrese valor de C: ";
 	cin >> c;
-	cout << "Ingrese un valor a evaluar: ";
-	cin >> v;
+	cout << "Ingrese un valor a evaluar(x): ";
+	cin >> x;
+	cout << endl;
 	dis = (b*b) - (4 * a * c);
 	if (dis <= 0)
 	{
@@ -22,7 +24,23 @@ int main()
 		system("pause");
 		return 0;
 	}
-	x1
-   cout << "Hello World!\n"; 
+	raiz = sqrt(dis);
+	cout << raiz;
+	r1 = (-b + raiz) / (2 * a);
+	r2 = (-b - raiz) / (2 * a);
+	formula = (a*(x*x)) + (b*x) + c;
+	cout << "Raiz 1: ";
+	cout << r1 << endl;
+	cout << "Raiz 2: ";
+	cout << r2 << endl;
+	cout << "Ax^2 + Bx + C = " << formula << endl;
+	system("pause");
+	return 0;
 }
-
+/*
+TEST
+Ingrese el valor de A: 3
+Ingrese el valor de B: 6
+Ingrese el valor de C: 1
+Ingrese un valor para evaluar: 10
+*/
